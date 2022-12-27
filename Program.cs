de1494d47d4bac824e8358925739ae67297e633e
singhtushar689 <singhtushar689@gmail.com>
2022-12-27 21:31:39 +0530

    Hello World

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StackAndQueueImolementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
